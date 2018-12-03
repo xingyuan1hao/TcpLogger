0fbf51f4bb8fe5ea758f2abd47f20d8a8f0f5e92
Hlin <ahhlin@163.com>
2018-12-03 19:25:56 +0800

    demo 2.0

---
+++ ver2.0/examples/loopsender.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "SocketLog.h"
+
+using std::cout;
+using std::endl;
+
+int main() {
+    cout<<"Send string every 1s..."<<endl;
+
+    auto socketLog = SocketLog::getInstance();
+
+    char buf[1024];
+    int count = 0;
+    while (true) {
+        sprintf(buf, "hello, count=%d\n", count++);
+        cout<<"try to send:"<<buf;
+        socketLog->post(buf);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    return 0;
+}
